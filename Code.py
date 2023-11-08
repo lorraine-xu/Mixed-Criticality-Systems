9b4e2d4502de23cef0f5f961f5f5280ade008c6a
Lorraine Xu <121460992+lorraine-xu@users.noreply.github.com>
2023-11-08 04:51:04 -0500

    importing taskset_generation

--- Code.py
+++ Code.py
@@ -1,7 +1,7 @@
 """Schedulability test implementation and partitioning algorithm comparisons."""
 
 import math
-from Mixed-Criticality-Systems.tasket_generation import generate_taskset
+from Mixed-Criticality-Systems.taskset_generation import generate_taskset
 
 class Task(object):
     """
@@ -33,17 +33,6 @@ def select_partitioning_heuristics():
 
     Output: four best partitioning heuristics
     """
-
-def generate_taskset():
-    """
-    Generate task utilizations for experiment. Available task generation algorithms are UUniFast
-    and the new algorithm proposed by Rodriguez et. al.
-
-    Parameters: UHI(τ), ULO(τ), the ratio of HI tasks (ratioHI = n ), the maximum and minimum
-    period nHI (tMax and tMin)
-    Output: a set of utilization values
-    """
-    print("generate_taskset() -- yet to be implemented.")
 
 def assign_priority(tasks):
     """
