a01aa345c8e2527629f515a54bf9402ff1d5e877
Lorraine Xu <121460992+lorraine-xu@users.noreply.github.com>
2023-11-08 04:48:21 -0500

    experiment

--- task_generation.py
+++ task_generation.py
@@ -1,2 +1,10 @@
-def generate_tasks():
-  # Generate a taskset of 1000 tasks.
+def generate_taskset():
+  """
+  Generate task utilizations for experiment. Available task generation algorithms are UUniFast
+  and the new algorithm proposed by Rodriguez et. al.
+
+  Parameters: UHI(τ), ULO(τ), the ratio of HI tasks (ratioHI = n ), the maximum and minimum
+  period nHI (tMax and tMin)
+  Output: a set of utilization values
+  """
+  print("generate_taskset() -- yet to be implemented.")
